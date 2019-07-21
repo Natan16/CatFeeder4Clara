7a126ed563a761c4f553b6c112be366ffe4d5d0d
Natan16 <natanvianat16@gmail.com>
2019-07-21 19:08:54 -0300

    Makes use of a table in order to determine the adequate amount of food to be given to the cat

--- comedouro/comedouro.ino
+++ comedouro/comedouro.ino
@@ -14,19 +14,46 @@
 //sends text message automatically to the petshop. Also, a report can be generated 
 //so the vet can have more information.
 #include <Servo.h>
+#include <math.h>
 
 Servo myservo;  // create servo object to control a servo
 // twelve servo objects can be created on most boards
 const int buttonPin = 13;
 const int buzzerPin = 3;
+
 long CycleTime = 28800000; //Time (in miliseconds) of the feeding cycle ( 8 hours )
 
 int buttonState = 1;  
 int prevButtonState = 1;
- 
-int pos = 0;    // variable to store the servo position
 
+
+//edit these values based on your case
+int catAgeInMonths = 4;
+int predictedAdultWeightInKg = 4;
+const float gramsPerPortion = 3.6;
+const int cyclesPerDay = 3;
+//according to the packet 
+int dailyFoodPerAge[3][11] = {{40 , 51 , 55 , 57 , 55 , 53 , 50 , 47 , 44 , 42 , 40},
+                   {47 , 60 , 65 , 67 , 65 , 63 , 60 , 56 , 54 , 51 , 50},
+                   {54 , 69 , 76 , 80 , 77 , 74 , 70 , 66 , 63 , 60 , 58} };              
+int numberOfPortionsPerDay = round(dailyFoodPerAge[predictedAdultWeightInKg - 3][catAgeInMonths - 2]/gramsPerPortion);
+
+int rest = numberOfPortionsPerDay%cyclesPerDay ;
+
+int numberOfPortionsPerFeedingCycle[cyclesPerDay];
+  
+
+int pos = 0;    // variable to store the servo position
 void setup() {
+  for(int i = 0  ; i < cyclesPerDay ; i++ ){
+    if ( rest > i){
+      numberOfPortionsPerFeedingCycle[i] = ceil(numberOfPortionsPerDay/cyclesPerDay) ;
+    }
+    else {
+      numberOfPortionsPerFeedingCycle[i] = floor(numberOfPortionsPerDay/cyclesPerDay) ;  
+    } 
+     
+  }
   pinMode(buttonPin , INPUT);
   pinMode(buzzerPin , OUTPUT);
   digitalWrite(buzzerPin , HIGH ) ;
@@ -48,40 +75,51 @@ void shake(){
 }
 
 
+void buzz(){
+   digitalWrite(buzzerPin , LOW);
+   delay(1000);
+   digitalWrite(buzzerPin , HIGH ) ; 
+}
+
 void feed(){
-    
-    digitalWrite(buzzerPin , LOW);
-    delay(1000);
-    digitalWrite(buzzerPin , HIGH ) ; 
     pos = 0 ;
     myservo.write(pos); 
     delay(1000);
     pos = 90;
     myservo.write(pos);
+    delay(1000);
 }
 
-
+int currentCycle = 0; 
 void loop() {
+Serial.println(dailyFoodPerAge[predictedAdultWeightInKg - 3][catAgeInMonths - 2]);
+
   buttonState = digitalRead(buttonPin);
   //Serial.println(buttonState);
   unsigned long CurrentTime = millis();
   unsigned long ElapsedTime = CurrentTime - StartTime;
   
+    //Serial.println(numberOfPortionsPerFeedingCycle);
   if ( buttonState == LOW && prevButtonState == HIGH ){
+    buzz(); 
     feed();
   }
   else if (ElapsedTime > CycleTime){
     shake();
-    feed();
+    buzz();
+    //feed as many times as the cat needs
+    for(int i = 0 ; i < numberOfPortionsPerFeedingCycle[currentCycle] ; i++){
+      feed();
+    }
+    currentCycle = (currentCycle + 1)%3;
     StartTime = millis();
-    Serial.println(ElapsedTime);
+    //Serial.println(ElapsedTime);
   }
   else {
     digitalWrite(buzzerPin , HIGH ) ; 
   }
   
   prevButtonState = buttonState;
-   
   
 }
 
